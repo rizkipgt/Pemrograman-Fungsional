255336f2c654dd98de74d1b4e548b7342aa66a3b
Rizki Pangestu <bangtutorial6528@gmail.com>
2024-06-09 20:16:20 +0700

    delete

--- resources/views/welcome.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.auth')
-
-@section('content')
-	<h1 class="text-center" style="margin-top: 200px;">Hello World!</h1>
-	<h2 class="subtitle text-center">This is your homepage</h2>
-@endsection
